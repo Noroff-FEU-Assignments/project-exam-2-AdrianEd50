a1297ee9e1cded95c01533fa138839aabf6d6501
AdrianEd50 <73480640+AdrianEd50@users.noreply.github.com>
2022-11-16 18:01:29 +0100

    updated register.js

--- project-exam-2/src/components/login/Register.js
+++ project-exam-2/src/components/login/Register.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Heading from "../layout/Heading";
-import RegisterForm from "./Register";
-
+import RegisterForm from "./RegisterForm";
 function Register() {
   return (
     <>
